02687576364cdb459ceabe163a3c2b45d8c1df7d
Kevin v Loon <74180193+KvnvL@users.noreply.github.com>
2024-04-10 16:48:42 +0200

    simple working connection with the OpenAPI api

--- src/pages/keywords/index.tsx
+++ src/pages/keywords/index.tsx
@@ -6,6 +6,7 @@ import KeywordSearching from "./searching";
 export default function Keywords() {
   const [pages, setPages] = useState(["collections"]);
   const page = pages[pages.length - 1];
+  const [keywordsArray, setKeywordsArray] = useState([]);
 
   const [searching, setSearching] = useState({
     subjects: [],
@@ -29,6 +30,41 @@ export default function Keywords() {
     ],
   });
 
+  // Start generating keywords
+  useEffect(() => {
+    if (searching.subjects.length > 0) {
+      generateKeywords();
+    }
+  }, [searching.subjects]);
+
+  // Reset the generated keywords
+  useEffect(() => {
+    if (pages[pages.length - 1] == "collections") {
+      setKeywordsArray([]);
+    }
+  }, [pages])
+
+  async function generateKeywords() {
+    console.log("Start generating");
+    try {
+      const response = await fetch("/api/generateKeywords", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ keywords: searching.subjects, location: "The Netherlands", wordsLength: ["shorttail", "longtail"]})
+      });
+
+      const data = await response.json();
+
+      setKeywordsArray(data.generatedKeywordsList);
+
+    } catch (error: any) {
+      console.error(error);
+      alert(error.message);
+    }
+  }
+
   if (page == "collections") {
     return (
       <CollectionsPage
@@ -38,7 +74,7 @@ export default function Keywords() {
       />
     );
   } else if (page == "search") {
-    return <KeywordSearching filters={searching} setPages={setPages} />;
+    return <KeywordSearching filters={searching} setPages={setPages} generatedKeywords={keywordsArray} />;
   } else {
     return null;
   }
--- src/pages/keywords/searching/index.tsx
+++ src/pages/keywords/searching/index.tsx
@@ -5,28 +5,38 @@ import InnerWrapper from "@/components/inner-wrapper/inner-wrapper.component";
 export default function KeywordSearching({
   filters,
   setPages,
+  generatedKeywords,
 }: {
   filters: any;
   setPages: any;
+  generatedKeywords?: any;
 }) {
   return (
     <InnerWrapper>
       <PageTitle
         title={"Search keywords"}
-        goBack={() => setPages((prevPages: any) => {
-          // Create a new array with all elements except the last one
-          return prevPages.slice(0, -1);
-        })}
+        goBack={() =>
+          setPages((prevPages: any) => {
+            // Create a new array with all elements except the last one
+            return prevPages.slice(0, -1);
+          })
+        }
       />
       <div className={styles.textWrapper}>
         <p>Input:</p>
-        {filters.subjects.map((subject : string) => 
+        {filters.subjects.map((subject: string) => (
           <p key={subject}>{subject}</p>
-        )}
+        ))}
       </div>
       <div className={styles.textWrapper}>
         <p>Generated keywords:</p>
-        
+        {generatedKeywords.length > 0 ? (
+          generatedKeywords.map((keyword: string) => (
+            <p key={keyword}>{keyword}</p>
+          ))
+        ) : (
+          <p>Loading...</p>
+        )}
       </div>
     </InnerWrapper>
   );
