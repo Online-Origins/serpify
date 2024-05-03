0add5592e8697f1dbb34bde49376cd593c93693e
KvnvL <kevin0409@kpnmail.nl>
2024-05-03 16:57:48 +0200

    first set-up for rich text content editor

--- src/app/content/create/writing/page.tsx
+++ src/app/content/create/writing/page.tsx
@@ -3,12 +3,18 @@ import { useEffect, useRef, useState } from "react";
 import styles from "./page.module.scss";
 import { supabase } from "@/app/api/supabaseClient/route";
 import InnerWrapper from "@/components/inner-wrapper/inner-wrapper.component";
-import PageTitle from "@/components/page-title/page-title.component";
+import { useEditor, EditorContent, EditorProvider } from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
 
 export default function Writing() {
   const [contentId, setContentId] = useState<any>("");
   const getContentRef = useRef(false);
+  const getOutlines = useRef(false);
   const [currentContent, setCurrentContent] = useState<any[]>([]);
+
+  const editor = useEditor({
+    extensions: [StarterKit],
+  });
 
   useEffect(() => {
     if (contentId != "" && !getContentRef.current) {
@@ -20,6 +26,19 @@ export default function Writing() {
     }
   });
 
+  useEffect(() => {
+    if (currentContent.length > 0 && !getOutlines.current) {
+      let content = "";
+
+      currentContent[0].outlines.map((outline: any) => {
+        content += `<${outline.type}>${outline.title}</${outline.type}><p></p>`;
+      });
+      editor?.commands.setContent(content);
+
+      getOutlines.current = true;
+    }
+  }, [currentContent, getOutlines]);
+
   async function getContent() {
     const { data } = await supabase
       .from("contentItems")
@@ -29,9 +48,15 @@ export default function Writing() {
       setCurrentContent(data);
     }
   }
+
   return (
     <InnerWrapper>
-      {currentContent.length > 0 && <h1>{currentContent[0].content_title}</h1>}
+      {currentContent.length > 0 && (
+        <div className={styles.editor}>
+          <h1>{currentContent[0].content_title}</h1>
+          <EditorContent editor={editor} />
+        </div>
+      )}
     </InnerWrapper>
   );
 }
