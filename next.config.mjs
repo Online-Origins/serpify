717bc4a0c5bccab38dc633919a0ddec42c195db8
denniemeijs334 <111436015+denniemeijs334@users.noreply.github.com>
2024-05-08 09:47:11 +0200

    use client

--- next.config.mjs
+++ next.config.mjs
@@ -17,7 +17,7 @@ const nextConfig = {
         // your project has type errors.
         // !! WARN !!
         ignoreBuildErrors: true,
-      },
+    }
 };
 
 export default nextConfig;
--- src/components/content-item/content-item.component.tsx
+++ src/components/content-item/content-item.component.tsx
@@ -1,8 +1,8 @@
+'use client'
 import classNames from "classnames";
 import styles from "./content-item.module.scss";
 import { CircularProgressbar } from "react-circular-progressbar";
 import BorderColorRoundedIcon from "@mui/icons-material/BorderColorRounded";
-import MoreVertIcon from "@mui/icons-material/MoreVert";
 import { useRouter } from "next/navigation";
 import DotsMenu from "../dots-menu/dots-menu.component";
 import { supabase } from "@/app/api/supabaseClient/route";
