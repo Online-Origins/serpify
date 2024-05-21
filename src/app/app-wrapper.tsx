c6b8c96479d2a1062857b32cec72b25f666e99ca
KvnvL <kevin0409@kpnmail.nl>
2024-05-21 14:21:18 +0200

    setup for content score

--- src/app/app-wrapper.tsx
+++ src/app/app-wrapper.tsx
@@ -1,10 +1,12 @@
-'use client';
+"use client";
+import PageTitle from "@/components/page-title/page-title.component";
 import styles from "./app-wrapper.module.scss";
 import MenuBar from "@/components/menu-bar/menu-bar.component";
 import ComponentWrapper from "@/components/ui/component-wrapper/component-wrapper.component";
 import classNames from "classnames";
 import { usePathname } from "next/navigation";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import ContentScore from "@/components/content-score/content-score.component";
 
 export default function AppWrapper({
   children,
@@ -12,7 +14,7 @@ export default function AppWrapper({
   children: React.ReactNode;
 }) {
   const pathname = usePathname();
-    const [smallNav, setSmallNav] = useState(pathname != "/");
+  const [smallNav, setSmallNav] = useState(pathname != "/");
 
   return (
     <body>
@@ -20,11 +22,17 @@ export default function AppWrapper({
       <ComponentWrapper
         className={classNames(
           styles.pagesWrapper,
-          !smallNav && styles.smallerWrapper
+          !smallNav && styles.smallerWrapper,
+          pathname == "/content/create/writing" && styles.small
         )}
       >
         <div className={styles.innerWrapper}>{children}</div>
       </ComponentWrapper>
+      {pathname == "/content/create/writing" && (
+        <ComponentWrapper className={styles.scoreWrapper}>
+          <ContentScore/>
+        </ComponentWrapper>
+      )}
     </body>
   );
 }
---
+++ src/components/content-score/content-score.component.tsx
@@ -0,0 +1,11 @@
+import PageTitle from '../page-title/page-title.component';
+import styles from './content-score.module.scss';
+
+export default function ContentScore() {
+
+    return (
+        <>
+            <PageTitle title={"Content score"} smallerHeader />
+        </>
+    )
+}
